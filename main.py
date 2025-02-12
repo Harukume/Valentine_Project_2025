11ffe1068454894a9806883da65707d963037202
Harukume <harusume1001@gmail.com>
2025-02-12 22:34:34 +0100

    Update: Final changes to project

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-from flask import Flask, redirect, url_for, render_template, request  #base for website
-from flask_bootstrap import Bootstrap5  # design for website
+# -*- coding: utf-8 -*-
+import smtplib
+import ssl
 import time
+from os import getenv
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+from dotenv import load_dotenv
+from flask import Flask, redirect, url_for, render_template, request, jsonify  # base for website
+from flask_bootstrap import Bootstrap5  # design for website
+
+load_dotenv()
+
+#getting env
+ADDR = getenv("ADDR")
+ADDR_PS = getenv("ADDR_PS")
+SNT = getenv("SNT")
+PORT = getenv("SMTP_PORT")
 
 #creating app with css
 app = Flask(__name__)
@@ -10,7 +26,7 @@ bootstrap = Bootstrap5(app)
 InfoToSend = {
     "TypeOfDate": "",
     "MainFood": "",
-    "Dessert": "",
+    "Dessert": "Ja robię",
 }
 
 
@@ -64,7 +80,7 @@ def food():
         elif request.form.get('Proposition') == 'Proposition':
             food = request.form.get('anotherChoice')
         else:
-            print("Error: Something is wrong")
+            print("Error: Something went wrong")
         print(f"Wybrał: {food}")
         InfoToSend["MainFood"] = food
         return redirect(url_for('dessert'))
@@ -72,10 +88,39 @@ def food():
     elif request.method == 'GET':
         return render_template("food.html")
 
-#TODO Co na deser
+#Dessert html with animations
 @app.route("/dessert")
 def dessert():
     return render_template("dessert.html")
-#TODO Wysyłanie odpowiedzi na maila
+
+@app.route("/message", methods=["GET", "POST"])
+def message():
+    """
+    Sends data from sites as an email.
+    :return: confirmation as json
+    """
+    message = f"""\
+    Wybrał randkę:{InfoToSend['TypeOfDate']}, \n\n
+    Jedzonko: {InfoToSend['MainFood']}
+
+    """
+    add_message = "Jeszcze żyje"
+    # Używamy MIME do utworzenia wiadomości
+    msg = MIMEMultipart()
+    msg['From'] = ADDR
+    msg['To'] = SNT
+    msg['Subject'] = f"Ankieta wynik"
+
+    # Dodajemy treść wiadomości
+    email_body = f"{message}\n\nPs. \n{add_message}"
+    msg.attach(MIMEText(email_body, 'plain', 'utf-8'))
+    #
+    with smtplib.SMTP_SSL("smtp.gmail.com", port=465) as connection:
+        connection.login(user=ADDR, password=ADDR_PS)
+        connection.sendmail(from_addr=ADDR, to_addrs=SNT, msg=msg.as_string())
+        print("Message sent")
+        connection.quit()
+
+    return jsonify({"message": "sent"})
 
 app.run(debug=True)
